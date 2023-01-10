abea8ff1d324723e5f5affa199eb421d0a42a5e8
melkamu <zmelkamu918@gmail.com>
2023-01-10 14:21:54 +0300

    relativelayout

--- app/src/main/java/com/example/onboardingpage/secondactivity.java
+++ app/src/main/java/com/example/onboardingpage/secondactivity.java
@@ -1,16 +1,13 @@
 package com.example.onboardingpage;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class secondactivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
 
     }
 }
